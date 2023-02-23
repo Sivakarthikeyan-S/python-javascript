19ec8adeedf796d8757d0f00bac3acb7b559a501
sivakarthi6055@gmail.com <sivakarthi6055@gmail.com>
2023-02-23 16:52:36 +0530

    cs50 python web sequence

--- cs50-python/functions-variables/calculator.py
+++ cs50-python/functions-variables/calculator.py
@@ -10,10 +10,15 @@
 def main():
     x = int(input("What's x? "))
     print("x squared is", square(x))
+    print("x cubed is", cube(x))
 
 
 def square(n):
     return pow(n, 2)
+
+
+def cube(n):
+    return pow(n, 3)
 
 
 main()
